e421211b70603d8390fda30ea1a0460f1281e2c1
FelixOe <felix90@ufl.edu>
2017-10-10 17:38:52 +0200

    Added redirects for dashboard.

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -1,8 +1,23 @@
 'use strict';
 
-angular.module('core').controller('HomeController', ['$scope', 'Authentication',
-  function ($scope, Authentication) {
+angular.module('core').controller('HomeController', ['$scope', '$state', 'Authentication', 'Profiles',
+  function ($scope, $state, Authentication, Profiles) {
     // This provides Authentication context.
     $scope.authentication = Authentication;
-  }
-]);
+	
+	//Redirect to login if user is not logged in
+    (function () {
+      if(!$scope.authentication.user)
+        $state.go('authentication.signin', $state.previous.params);
+    })();
+  
+	//Redirect to profile creation if user has not created a profile
+    (function () {
+      var profile = Profiles.get({
+        user: $scope.authentication.user._id
+      }, function(profile, response){
+        if(!profile._id)
+          $state.go('profile.create', $state.previous.params);
+      });
+    })();
+  }]);
