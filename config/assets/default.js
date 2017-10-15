273e989a2dd9827be7adf891b0e8c0cf7504f071
nschwalb1 <neilschwalb@gmail.com>
2017-10-15 00:26:36 -0400

    Added angular smart-table dependency

--- config/assets/default.js
+++ config/assets/default.js
@@ -20,7 +20,7 @@ module.exports = {
         'public/lib/owasp-password-strength-test/owasp-password-strength-test.js',
         'public/lib/angular-ui-sortable/sortable.js',
         'public/lib/angular-ui-slider/src/slider.js',
-        'public/lib/angular-smart-table/smart-table.js'
+        'public/lib/angular-smart-table/dist/smart-table.js'
       ],
       tests: ['public/lib/angular-mocks/angular-mocks.js']
     },
