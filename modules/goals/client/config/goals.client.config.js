f1fc3515eb84a70954da5f9d53037c9f0511e301
FelixOe <felix90@ufl.edu>
2017-10-27 18:44:49 +0200

    Added missing semicolons, changed formatting of priority service

--- modules/goals/client/config/goals.client.config.js
+++ modules/goals/client/config/goals.client.config.js
@@ -2,5 +2,5 @@
   'use strict';
 
   angular
-    .module('goals')
+    .module('goals');
     }());
--- modules/goals/client/services/priority.client.service.js
+++ modules/goals/client/services/priority.client.service.js
@@ -5,28 +5,28 @@
 //TODO: Currently untested. Once profile was adjusted, test and finish this service
 angular.module('goals').factory('PriorityService', [
   function () {
-	return {
-		getPriority: function(profile, category) {
-			var priority = profile.Priority[0][category];
-			var satisfaction = profile.Satisfaction[0][category];
+    return {
+      getPriority: function(profile, category) {
+        var priority = profile.Priority[0][category];
+        var satisfaction = profile.Satisfaction[0][category];
 			
-			var result = '';
+        var result = '';
 			
-			if(priority >= 1 && priority <= 5 && satisfaction >= 0 && satisfaction <= 4) {
-				result = "Support";
-			}
-			else if(priority >= 1 && priority <= 5 && satisfaction >= 5 && satisfaction <= 10) {
-				result = "Maintenance";
-			}
-			else if(priority >= 6 && priority <= 10 && satisfaction >= 0 && satisfaction <= 4) {
-				result = "Cut/Shift";
-			}
-			else if(priority >= 6 && priority <= 10 && satisfaction >= 5 && satisfaction <= 10) {
-				result = "Minimize";
-			}
+        if(priority >= 1 && priority <= 5 && satisfaction >= 0 && satisfaction <= 4) {
+          result = 'Support';
+        }
+        else if(priority >= 1 && priority <= 5 && satisfaction >= 5 && satisfaction <= 10) {
+          result = 'Maintenance';
+        }
+        else if(priority >= 6 && priority <= 10 && satisfaction >= 0 && satisfaction <= 4) {
+          result = 'Cut/Shift';
+        }
+        else if(priority >= 6 && priority <= 10 && satisfaction >= 5 && satisfaction <= 10) {
+          result = 'Minimize';
+        }
 			
-			return result;
-		}
-	}
+        return result;
+      }
+    };
   }
 ]);
