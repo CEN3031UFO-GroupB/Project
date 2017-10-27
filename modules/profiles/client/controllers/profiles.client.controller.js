8663a799839a4a8724994e37ef79d72afaad5aab
Jake Loredo <jake.loredo@gmail.com>
2017-10-27 18:08:07 -0400

    Updated Profiles schema and modified client/user controller and views to use the new schema

--- modules/profiles/client/controllers/profiles.client.controller.js
+++ modules/profiles/client/controllers/profiles.client.controller.js
@@ -30,21 +30,27 @@ angular.module('profiles').controller('ProfilesController', ['$scope', '$statePa
                     name: 'Contribution'
                 },
                 {
-                    name: 'Security'
+                    name: 'Personal Growth'
                 },
                 {
-                    name: 'Personal Growth'
-                }
+                    name: 'Career'
+                },
+                {
+                    name: 'Physical Environment'
+                },
             ],
 			satisfactions: {
-				personalGrowth: 5,
-				career: 5,
-				familyAndFriends: 5,
-				health: 5,
-				physicalEnv: 5,
-				romance: 5,
-				money: 5,
-				fun: 5
+                Family: 5,
+                Health: 5,
+                Rest_and_Relaxation: 5,
+                Faith: 5,
+                Finance: 5,
+                Romance: 5,
+                Friends: 5,
+                Contribution: 5,
+                Personal_Growth: 5,
+                Career: 5,
+                Physical_Environment: 5
 			}
         };
 
@@ -64,19 +70,11 @@ angular.module('profiles').controller('ProfilesController', ['$scope', '$statePa
                         Romance: $scope.profile.priorities.findIndex(x => x.name === "Romance") + 1,
                         Friends: $scope.profile.priorities.findIndex(x => x.name === "Friends") + 1,
                         Contribution: $scope.profile.priorities.findIndex(x => x.name === "Contribution") + 1,
-                        Security: $scope.profile.priorities.findIndex(x => x.name === "Security") + 1,
-                        Personal_Growth: $scope.profile.priorities.findIndex(x => x.name === "Personal Growth") + 1
+                        Personal_Growth: $scope.profile.priorities.findIndex(x => x.name === "Personal Growth") + 1,
+                        Career: $scope.profile.priorities.findIndex(x => x.name === "Career") + 1,
+                        Physical_Environment: $scope.profile.priorities.findIndex(x => x.name === "Physical Environment") + 1,
                     },
-                    Satisfaction: {
-                        Personal_Growth: $scope.profile.satisfactions.personalGrowth,
-                        Career: $scope.profile.satisfactions.career,
-                        Family_and_Friends: $scope.profile.satisfactions.familyAndFriends,
-                        Health: $scope.profile.satisfactions.health,
-                        Physical_Env: $scope.profile.satisfactions.physicalEnv,
-                        Romance: $scope.profile.satisfactions.romance,
-                        Money: $scope.profile.satisfactions.money,
-                        Fun: $scope.profile.satisfactions.fun
-                    }
+                    Satisfaction: $scope.profile.satisfactions
                 });
     						
                 // Redirect after save
@@ -105,19 +103,11 @@ angular.module('profiles').controller('ProfilesController', ['$scope', '$statePa
                     Romance: $scope.profile.priorities.findIndex(x => x.name === "Romance") + 1,
                     Friends: $scope.profile.priorities.findIndex(x => x.name === "Friends") + 1,
                     Contribution: $scope.profile.priorities.findIndex(x => x.name === "Contribution") + 1,
-                    Security: $scope.profile.priorities.findIndex(x => x.name === "Security") + 1,
-                    Personal_Growth: $scope.profile.priorities.findIndex(x => x.name === "Personal Growth") + 1
+                    Personal_Growth: $scope.profile.priorities.findIndex(x => x.name === "Personal Growth") + 1,
+                    Career: $scope.profile.priorities.findIndex(x => x.name === "Career") + 1,
+                    Physical_Environment: $scope.profile.priorities.findIndex(x => x.name === "Physical Environment") + 1,
                 };
-                profile.Satisfaction = {
-                    Personal_Growth: $scope.profile.satisfactions.personalGrowth,
-                    Career: $scope.profile.satisfactions.career,
-                    Family_and_Friends: $scope.profile.satisfactions.familyAndFriends,
-                    Health: $scope.profile.satisfactions.health,
-                    Physical_Env: $scope.profile.satisfactions.physicalEnv,
-                    Romance: $scope.profile.satisfactions.romance,
-                    Money: $scope.profile.satisfactions.money,
-                    Fun: $scope.profile.satisfactions.fun
-                };
+                profile.Satisfaction = $scope.profile.satisfactions;
 
                 profile.$update(function () {
                     $location.path('profile/view');
@@ -134,11 +124,11 @@ angular.module('profiles').controller('ProfilesController', ['$scope', '$statePa
                 
                 // Function for the view/edit profile view
                 // Needs to be here so that it only gets called after the get() query finishes
-                function(s){
+                function(result){
 
                     // Make the Priority object into an array
                     // Arrays are much easier to display with ng-repeat
-                    var p = s.Priority[0];
+                    var p = result.Priority[0];
                     $scope.prioritiesArray = [];
                     angular.forEach(p, function(value, key) {
                         if(key !== '_id'){
@@ -151,26 +141,20 @@ angular.module('profiles').controller('ProfilesController', ['$scope', '$statePa
                     });
 
                     // Set the scroller's values to those on the user's profile
-                    $scope.profile.satisfactions.personalGrowth = s.Satisfaction[0].Personal_Growth;
-                    $scope.profile.satisfactions.career = s.Satisfaction[0].Career;
-                    $scope.profile.satisfactions.familyAndFriends = s.Satisfaction[0].Family_and_Friends;
-                    $scope.profile.satisfactions.health = s.Satisfaction[0].Health;
-                    $scope.profile.satisfactions.physicalEnv = s.Satisfaction[0].Physical_Env;
-                    $scope.profile.satisfactions.romance = s.Satisfaction[0].Romance;
-                    $scope.profile.satisfactions.money = s.Satisfaction[0].Money;
-                    $scope.profile.satisfactions.fun = s.Satisfaction[0].Fun;
+                    console.log(result.Satisfaction[0]);
+                    $scope.profile.satisfactions = result.Satisfaction[0];
                     
                     // Check if it's been 12 weeks since last changed
                     var oneDay = 24*60*60*1000;
-                    var dateOnProfile = new Date(s.last_modified);
+                    var dateOnProfile = new Date(result.last_modified);
                     var daysElapsed = Math.round(Math.abs((dateOnProfile.getTime() - (new Date()).getTime())/(oneDay)));
 
                     // Check if the profile is less than 1 day old (grace period)
-                    var createdOn = new Date(s.created_on);
+                    var createdOn = new Date(result.created_on);
                     var daysSinceCreated = Math.round(Math.abs((createdOn.getTime() - (new Date()).getTime())/(oneDay)));
                     
                     // This variable will be used with ng-show on the view to indicate when it's time to update the profile
-                    $scope.profileCanChange = (daysElapsed >= 7*12 || createdOn =< 1);
+                    $scope.profileCanChange = (daysElapsed >= 7*12 || createdOn <= 1);
             });
         };
 
