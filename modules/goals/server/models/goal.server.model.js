d4d27d43ebce189ca653801dd85cbacc3b443a8c
nschwalb1 <neilschwalb@gmail.com>
2017-10-30 20:48:21 -0400

    Created new schema for Goals list and historical goals

--- modules/goals/server/models/goal.server.model.js
+++ modules/goals/server/models/goal.server.model.js
@@ -7,23 +7,58 @@ var mongoose = require('mongoose'),
   Schema = mongoose.Schema;
 
 /**
- * Goal Schema
- */
-var GoalSchema = new Schema({
-  name: {
-    type: String,
-    default: '',
-    required: 'Please fill Goal name',
-    trim: true
-  },
-  created: {
-    type: Date,
-    default: Date.now
-  },
+ * Individual goals schema
+*/
+var GoalsSchema = new Schema({
   user: {
     type: Schema.ObjectId,
     ref: 'User'
+  },
+  title: {
+    type: String,
+    default: '',
+    required: 'Please add a goal title',
+    trim: true
+  },
+  description: {
+    type: String,
+    default: '',
+    required: 'Please add a goal description'
+  },
+  category: {
+    type: String,
+    required: 'Please add a category for your goal'
+  },
+  status: {
+    type: String,
+    default: 'Not Started',
+    enum: ['Not Started', 'In Progress', 'Completed'],
+    required: 'Please set a status for the goal'
+  },
+  created_at: {
+    type: Date,
+    default: Date.now
+  },
+  updated_at: {
+    type: Date,
+    default: Date.now
+  },
+  week_timestamp: {
+    type: Date,
+    default: Date.now
   }
 });
 
-mongoose.model('Goal', GoalSchema);
+/**
+ * GoalsList Schema
+ */
+var GoalsListSchema = new Schema({
+  user: {
+    type: Schema.ObjectId,
+    ref: 'User'
+  },
+  goals: [GoalsSchema]
+});
+
+mongoose.model('GoalsList', GoalsListSchema);
+mongoose.model('Goals', GoalsSchema);
