a32925218f0a7b24a91b4a135dab618b3a8a534f
nschwalb1 <neilschwalb@gmail.com>
2017-10-30 20:53:34 -0400

    Rename 'Goals' schema to 'Goal'

--- modules/goals/server/controllers/goals.server.controller.js
+++ modules/goals/server/controllers/goals.server.controller.js
@@ -6,6 +6,7 @@
 var path = require('path'),
   mongoose = require('mongoose'),
   Goal = mongoose.model('Goal'),
+  GoalsList = mongoose.model('GoalsList'),
   errorHandler = require(path.resolve('./modules/core/server/controllers/errors.server.controller')),
   _ = require('lodash');
 
