bbd2941d186f7c3c12a2cdce2805db977b725302
nschwalb1 <neilschwalb@gmail.com>
2017-11-01 23:51:44 -0400

    Removed angular.filter

--- modules/core/client/app/config.js
+++ modules/core/client/app/config.js
@@ -4,7 +4,7 @@
 var ApplicationConfiguration = (function () {
   // Init module configuration options
   var applicationModuleName = 'mean';
-  var applicationModuleVendorDependencies = ['ngResource', 'ngAnimate', 'ngMessages', 'ui.router', 'ui.bootstrap', 'ui.utils', 'angularFileUpload', 'smart-table', 'angular.filter'];
+  var applicationModuleVendorDependencies = ['ngResource', 'ngAnimate', 'ngMessages', 'ui.router', 'ui.bootstrap', 'ui.utils', 'angularFileUpload', 'smart-table'];
 
   // Add a new vertical module
   var registerModule = function (moduleName, dependencies) {
