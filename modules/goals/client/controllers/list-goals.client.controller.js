ce5546ba363391297d960b0a6b03e11edaae4703
nschwalb1 <neilschwalb@gmail.com>
2017-11-02 23:43:26 -0400

    Developed list view for goals

--- modules/goals/client/controllers/list-goals.client.controller.js
+++ modules/goals/client/controllers/list-goals.client.controller.js
@@ -9,7 +9,35 @@
 
   function GoalsListController($scope, $state, $window, Authentication, GoalsService) {
     var vm = this;
-    $scope.status = ['Complete', 'In Progress', 'Not Started'];
     vm.goals = GoalsService.query();
+
+    function getThisMonday () {
+      var d = new Date();
+      var day = d.getDay();
+      var diff = d.getDate() - day + (day == 0 ? -6:1);
+      var monday = new Date(d.setDate(diff));
+      monday.setHours(0, 0, 0, 0)
+      return monday;
+    }
+    $scope.monday = getThisMonday();
+
+    $scope.status = ['Complete', 'In Progress', 'Not Started'];
+
+     $scope.createGoal = function () {
+      $state.go('goals.create');
+    }
+
+    $scope.markGoalInProgress = function (goal) {
+      goal.status = 'In Progress';
+      console.log(JSON.stringify(goal));
+      GoalsService.update(goal);
+    }
+
+    $scope.markGoalComplete = function (goal) {
+      goal.status = 'Complete';
+      console.log(JSON.stringify(goal));
+      GoalsService.update(goal);
+    }
+
   }
 }());
