1cc504b15060fd11eb3c058e3c222b98d68fa4b9
nschwalb1 <neilschwalb@gmail.com>
2017-11-02 23:40:38 -0400

    Added goals to the top menu

--- modules/goals/client/config/goals.client.config.js
+++ modules/goals/client/config/goals.client.config.js
@@ -1,6 +1,11 @@
-(function () {
-  'use strict';
+'use strict';
 
-  angular
-    .module('goals');
-    }());
+  angular.module('goals').run(['Menus',
+    function (Menus) {
+      // Set top bar menu items
+      Menus.addMenuItem('topbar', {
+        title: 'Goals',
+        state: 'goals.list'
+      });
+    }
+  ]);
