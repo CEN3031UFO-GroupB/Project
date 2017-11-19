61e89b428104179b30679db6a3e37d517082b407
nschwalb1 <neilschwalb@gmail.com>
2017-11-19 15:49:24 -0500

    Added performance service

--- modules/goals/client/controllers/list-goals.client.controller.js
+++ modules/goals/client/controllers/list-goals.client.controller.js
@@ -5,16 +5,28 @@
     .module('goals')
     .controller('GoalsListController', GoalsListController);
 
-  GoalsListController.$inject = ['$scope', '$state', '$window', 'Authentication', 'GoalsService', 'PriorityService', 'Profiles'];
+  GoalsListController.$inject = ['$scope', '$state', '$window', 'Authentication', 'GoalsService', 'PriorityService', 'PerformanceService', 'Profiles'];
 
-  function GoalsListController($scope, $state, $window, Authentication, GoalsService, PriorityService, Profiles) {
+  function GoalsListController($scope, $state, $window, Authentication, GoalsService, PriorityService, PerformanceService, Profiles) {
     var vm = this;
+    vm.oldGoals = [];
+
+    function getThisMonday() {
+      var d = new Date();
+      var day = d.getDay();
+      var diff = d.getDate() - day + (day == 0 ? -6:1);
+      var monday = new Date(d.setDate(diff));
+      monday.setHours(0, 0, 0, 0);
+      return monday;
+    }
 
 	//Add priorities to goals
     (function() {
       GoalsService.query().$promise.then(function(value) {
         vm.goals = value;
         var userId = '';
+
+        PerformanceService.getPerformance(vm.goals);
 
         if(vm.goals.length > 0)
         {				
@@ -26,20 +38,18 @@
             for(var i = 0; i < vm.goals.length; i++) {
               var priority = PriorityService.getPriority($scope.userProfile, vm.goals[i].category);
               vm.goals[i].priority = priority;
+              if(vm.goals[i].completed_at){
+                if(new Date(vm.goals[i].completed_at).getTime() < getThisMonday().getTime()){
+                  vm.oldGoals.push(vm.goals[i]);
+                }
+              }
             }
           });
         }
       });
     })();
 
-    function getThisMonday() {
-      var d = new Date();
-      var day = d.getDay();
-      var diff = d.getDate() - day + (day == 0 ? -6:1);
-      var monday = new Date(d.setDate(diff));
-      monday.setHours(0, 0, 0, 0);
-      return monday;
-    }
+
     $scope.monday = getThisMonday();
     var today = new Date();
     var timeDiff = Math.abs(today.getTime() - $scope.monday.getTime());
